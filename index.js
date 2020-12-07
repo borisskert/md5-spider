d4e78895ad169dc48e34dd9078cfe48decdc6125
Boris Skert <boris.skert@gmail.com>
2020-12-07 21:59:37 +0100

    Update spider

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const spider = require('./src/spider');
 
 (function () {
-    spider().then(md5 => console.log(md5));
+    spider();
 })();
--- src/spider.js
+++ src/spider.js
@@ -1,13 +1,40 @@
-const settings = require("./settings");
-const get = require("./get");
-const checksum = require("./checksum");
+const readSettings = require('./settings');
+const get = require('./get');
+const checksum = require('./checksum');
+const store = require('./store')
 
-const doIt = async () => {
-    return await settings.read().then(settings => {
-        return get(settings.url);
-    }).then(content => {
-        return checksum(content);
-    });
+const spider = () => {
+    const settings = readSettings.read();
+    const url = settings.url;
+
+    const checkSite = () => {
+        const content = get(url);
+        const newChecksum = checksum(content);
+
+        const existingChecksum = store.read(url);
+
+        return {
+            newChecksum,
+            existingChecksum
+        }
+    }
+
+    const check = () => {
+        const {newChecksum, existingChecksum} = checkSite();
+
+        if (existingChecksum !== newChecksum) {
+            store.write(url, newChecksum);
+            console.log(`Checksum for ${url} changed: ${existingChecksum} => ${newChecksum}`);
+        } else {
+            console.log(`Checksum for ${url} unchanged: ${existingChecksum}`);
+        }
+    }
+
+    check();
+
+    setInterval(() =>{
+        check();
+    }, settings.check_interval * 1000);
 }
 
-module.exports = doIt;
+module.exports = spider;
